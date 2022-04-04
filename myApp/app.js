a014bc323938125d47a3740526c92cef8c2df2a3
ValentinDelPino <vale.del.pino02@gmail.com>
2022-04-04 15:35:11 -0300

    pase todos los html a ejs, conecte el css a todos los ejs y cree el formulario de login

--- myApp/app.js
+++ myApp/app.js
@@ -5,8 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 const productsRouters = require('./routes/products');     //Nueva ruta
+const loginRouter = require('./routes/users');     //Nueva ruta
+
 
 var app = express();
 
@@ -22,8 +23,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //Listeners
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/products', productsRouters);     //Ruta nueva
+app.use('/login', loginRouter);     //Ruta nueva
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
