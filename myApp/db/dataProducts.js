2579c60293201c6fe65304208abaaa8d686ed44b
vdelpino <vdelpino@udesa.edu.ar>
2022-04-05 17:18:11 -0300

    armando la base de datos de nuestros productos

--- myApp/db/dataProducts.js
+++ myApp/db/dataProducts.js
@@ -1,9 +1,48 @@
-// let keyboards = {
-//     lista: [{
-//         saludo: "Hola Mundo"
-//     }]
-// }
+ let periferics = {
+    lista: [
+        {
+            titulo: "Teclado gamer Marvo KG901 QWERTY Jixian Blue inglés US color negro con luz rainbow",
+            descripcion: "Este teclado Marvo de alto rendimiento permite que puedas disfrutar de horas ilimitadas de juegos. Está diseñado especialmente para que puedas expresar tanto tus habilidades como tu estilo. Podrás mejorar tu experiencia de gaming, ya seas un aficionado o todo un experto y hacer que tus jugadas alcancen otro nivel.",
+            img: "/images/products/marvo.jpg"
+        },{
+            titulo: "Teclado gamer Redragon Dark Avenger K568 QWERTY Outemu Red español color negro con luz rainbow",
+            descripcion: "Disfrutá de tus partidas en otro nivel con Redragon, marca reconocida que se especializa en brindar la mejor experiencia de juego al público gamer desde hace más de 20 años. Sus teclados se adaptan a todo tipo de jugadores y esto los convierten en un fiel reflejo de la alta gama y calidad que la compañía ofrece.",
+            img: "/images/products/dark-avenger.jpg"
+        },{
+            titulo: "Teclado gamer Redragon Kumara K552 QWERTY Outemu Blue español latinoamérica color negro con luz RGB",
+            descripcion: "La gran calidad del Redragon Kumara K552, y su precio económico lo vuelven un atractivo ideal para que te diviertas frente a la pantalla. Su ergonomía, su base antidelizante y su rápido tiempo de respuesta permite que tus juegos favoritos se sientan más cerca que nunca, al alcance de tus manos.",
+            img: "/images/products/kumara-k552.jpg"
+        },{
+            titulo: "Mouse de juego Logitech G Series Hero G502 kda",
+            descripcion: "Logitech diseña productos y experiencias que ocupan un lugar cotidiano en la vida de las personas, poniendo foco en la innovación y la calidad. Su objetivo es crear momentos verdaderamente únicos y significativos para sus usuarios. Los mouses Logitech se adaptan a la forma de tu mano para proporcionarte horas de comodidad. Sin necesidad de mover el brazo para deslizar el cursor, tu mano se fatigará menos. Son ideales para cualquier espacio de trabajo y quienes tienen la mesa llena de diversos objetos.",
+            img: "/images/products/g502.jpg"
+        },{
+            titulo: "Mouse de juego Logitech G Series Lightsync G203 negro",
+            descripcion: "Logitech diseña productos y experiencias que ocupan un lugar cotidiano en la vida de las personas, poniendo foco en la innovación y la calidad. Su objetivo es crear momentos verdaderamente únicos y significativos para sus usuarios. Los mouses Logitech se adaptan a la forma de tu mano para proporcionarte horas de comodidad. Sin necesidad de mover el brazo para deslizar el cursor, tu mano se fatigará menos. Son ideales para cualquier espacio de trabajo y quienes tienen la mesa llena de diversos objetos.",
+            img: "/images/products/g203.jpg"
+        },{
+            titulo: "Teclado gamer Marvo KG901 QWERTY Jixian Blue inglés US color negro con luz rainbow",
+            descripcion: "Este teclado Marvo de alto rendimiento permite que puedas disfrutar de horas ilimitadas de juegos. Está diseñado especialmente para que puedas expresar tanto tus habilidades como tu estilo. Podrás mejorar tu experiencia de gaming, ya seas un aficionado o todo un experto y hacer que tus jugadas alcancen otro nivel.",
+            img: "/images/products/marvo.jpg"
+        },{
+            titulo: "Teclado gamer Marvo KG901 QWERTY Jixian Blue inglés US color negro con luz rainbow",
+            descripcion: "Este teclado Marvo de alto rendimiento permite que puedas disfrutar de horas ilimitadas de juegos. Está diseñado especialmente para que puedas expresar tanto tus habilidades como tu estilo. Podrás mejorar tu experiencia de gaming, ya seas un aficionado o todo un experto y hacer que tus jugadas alcancen otro nivel.",
+            img: "/images/products/marvo.jpg"
+        },{
+            titulo: "Teclado gamer Marvo KG901 QWERTY Jixian Blue inglés US color negro con luz rainbow",
+            descripcion: "Este teclado Marvo de alto rendimiento permite que puedas disfrutar de horas ilimitadas de juegos. Está diseñado especialmente para que puedas expresar tanto tus habilidades como tu estilo. Podrás mejorar tu experiencia de gaming, ya seas un aficionado o todo un experto y hacer que tus jugadas alcancen otro nivel.",
+            img: "/images/products/marvo.jpg"
+        },{
+            titulo: "Teclado gamer Marvo KG901 QWERTY Jixian Blue inglés US color negro con luz rainbow",
+            descripcion: "Este teclado Marvo de alto rendimiento permite que puedas disfrutar de horas ilimitadas de juegos. Está diseñado especialmente para que puedas expresar tanto tus habilidades como tu estilo. Podrás mejorar tu experiencia de gaming, ya seas un aficionado o todo un experto y hacer que tus jugadas alcancen otro nivel.",
+            img: "/images/products/marvo.jpg"
+        },{
+            titulo: "Teclado gamer Marvo KG901 QWERTY Jixian Blue inglés US color negro con luz rainbow",
+            descripcion: "Este teclado Marvo de alto rendimiento permite que puedas disfrutar de horas ilimitadas de juegos. Está diseñado especialmente para que puedas expresar tanto tus habilidades como tu estilo. Podrás mejorar tu experiencia de gaming, ya seas un aficionado o todo un experto y hacer que tus jugadas alcancen otro nivel.",
+            img: "/images/products/marvo.jpg"
+        }
+    ]
+}
 
-let keyboards = 'Hola Mundo';
 
-module.exports = keyboards;
+module.exports = periferics;
