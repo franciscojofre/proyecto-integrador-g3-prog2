23ef4b091f3dea230d9743f940bb410c09398098
vdelpino <vdelpino@udesa.edu.ar>
2022-04-08 19:58:17 -0300

    solucione problema con el id de los productos de dataProducts, cambie toda la apariencia de la pagina para tener un test de como puede ir quedando, le faltan ajustes y mas miradas, cambie el logo tanto el main como el favicon, tambien el banner home para darle un aspecto mas relacionado a lo que va la pagina, y le di un fondo a la pagina con una imagen para quitar el fondo blanco

--- myApp/db/dataProducts.js
+++ myApp/db/dataProducts.js
@@ -1,144 +1,144 @@
  let periferics = {
     lista: [
         {
-            titulo: "Teclado gamer Marvo KG901 QWERTY Jixian Blue inglés US color negro con luz rainbow",
+            titulo: "Teclado gamer Marvo KG901",
             descripcion: "Este teclado Marvo de alto rendimiento permite que puedas disfrutar de horas ilimitadas de juegos. Está diseñado especialmente para que puedas expresar tanto tus habilidades como tu estilo. Podrás mejorar tu experiencia de gaming, ya seas un aficionado o todo un experto y hacer que tus jugadas alcancen otro nivel.",
             img: "/images/products/marvo.jpg",
             id: 1,
             comentarios: 11,
             novedad: false
         },{
-            titulo: "Teclado gamer Redragon Dark Avenger K568 QWERTY Outemu Red español color negro con luz rainbow",
+            titulo: "Teclado Redragon Dark Avenger K568",
             descripcion: "Disfrutá de tus partidas en otro nivel con Redragon, marca reconocida que se especializa en brindar la mejor experiencia de juego al público gamer desde hace más de 20 años. Sus teclados se adaptan a todo tipo de jugadores y esto los convierten en un fiel reflejo de la alta gama y calidad que la compañía ofrece.",
             img: "/images/products/dark-avenger.jpg",
             id: 2,
             comentarios: 14,
             novedad: false
         },{
-            titulo: "Teclado gamer Redragon Kumara K552 QWERTY Outemu Blue español latinoamérica color negro con luz RGB",
+            titulo: "Teclado Redragon Kumara K552",
             descripcion: "La gran calidad del Redragon Kumara K552, y su precio económico lo vuelven un atractivo ideal para que te diviertas frente a la pantalla. Su ergonomía, su base antidelizante y su rápido tiempo de respuesta permite que tus juegos favoritos se sientan más cerca que nunca, al alcance de tus manos.",
             img: "/images/products/kumara-k552.jpg",
             id: 3,
             comentarios: 17,
             novedad: false
         },{
-            titulo: "Mouse de juego Logitech G Series Hero G502 kda",
+            titulo: "Mouse Logitech G502",
             descripcion: "Logitech diseña productos y experiencias que ocupan un lugar cotidiano en la vida de las personas, poniendo foco en la innovación y la calidad. Su objetivo es crear momentos verdaderamente únicos y significativos para sus usuarios. Los mouses Logitech se adaptan a la forma de tu mano para proporcionarte horas de comodidad. Sin necesidad de mover el brazo para deslizar el cursor, tu mano se fatigará menos. Son ideales para cualquier espacio de trabajo y quienes tienen la mesa llena de diversos objetos.",
             img: "/images/products/g502.jpg",
             id: 4,
             comentarios: 19,
             novedad: false
         },{
-            titulo: "Mouse de juego Logitech G Series Lightsync G203 negro",
+            titulo: "Mouse Logitech G203",
             descripcion: "Logitech diseña productos y experiencias que ocupan un lugar cotidiano en la vida de las personas, poniendo foco en la innovación y la calidad. Su objetivo es crear momentos verdaderamente únicos y significativos para sus usuarios. Los mouses Logitech se adaptan a la forma de tu mano para proporcionarte horas de comodidad. Sin necesidad de mover el brazo para deslizar el cursor, tu mano se fatigará menos. Son ideales para cualquier espacio de trabajo y quienes tienen la mesa llena de diversos objetos.",
             img: "/images/products/g203.jpg",
             id: 5,
             comentarios: 13,
             novedad: false
         },{
-            titulo: "Auriculares gamer inalámbricos Logitech G Series G733 negro con luz rgb LED",
+            titulo: "Auriculares Logitech G733",
             descripcion: "¡Experimentá la adrenalina de sumergirte en la escena de otra manera! Tener auriculares específicos para jugar cambia completamente tu experiencia en cada partida. Con los Logitech G733 no te perdés ningún detalle y escuchás el audio tal y como fue diseñado por los creadores.",
             img: "/images/products/logitech-gseries.jpg",
             id: 6,
             comentarios: 12,
             novedad: false
         },{
-            titulo: "Teclado gamer HyperX Alloy Origins Core QWERTY Red inglés US color negro con luz RGB",
+            titulo: "Teclado HyperX Alloy Origins Core",
             descripcion: "Este teclado HyperX de alto rendimiento permite que puedas disfrutar de horas ilimitadas de juegos. Está diseñado especialmente para que puedas expresar tanto tus habilidades como tu estilo. Podrás mejorar tu experiencia de gaming, ya seas un aficionado o todo un experto y hacer que tus jugadas alcancen otro nivel.",
             img: "/images/products/hyperx-alloy.jpg",
             id: 7,
             comentarios: 21,
             novedad: false
         },{
-            titulo: "Teclado gamer bluetooth Redragon Draconic K530 QWERTY Outemu Brown inglés US color blanco con luz RGB",
+            titulo: "Teclado Redragon Draconic K530",
             descripcion: "Disfrutá de tus partidas en otro nivel con Redragon, marca reconocida que se especializa en brindar la mejor experiencia de juego al público gamer desde hace más de 20 años. Sus teclados se adaptan a todo tipo de jugadores y esto los convierten en un fiel reflejo de la alta gama y calidad que la compañía ofrece.",
             img: "/images/products/k530.jpg",
             id: 8,
             comentarios: 15,
             novedad: false
         },{
-            titulo: "Auriculares gamer HyperX Cloud Stinger S negro",
+            titulo: "Auriculares HyperX Cloud Stinger S",
             descripcion: "¡Experimentá la adrenalina de sumergirte en la escena de otra manera! Tener auriculares específicos para jugar cambia completamente tu experiencia en cada partida. Con los HyperX Stinger S no te perdés ningún detalle y escuchás el audio tal y como fue diseñado por los creadores.",
             img: "/images/products/cloud-stinger.jpg",
             id: 9,
             comentarios: 7,
             novedad: true
         },{
-            titulo: "Auriculares Gamer Redragon Zeus X Usb Rgb 7.1 Pc Full",
+            titulo: "Auriculares Redragon Zeus X",
             descripcion: "Potentes, hermosos, totales. Con retroiluminación RGB, un micrófono excepcional, construcción cómoda y robusta, y -lo más importante- una calidad de sonido arrolladora, los aclamados Zeus vuelven con más: conoce su nueva versión X H510-RGB y experimenta la totalidad.",
             img: "/images/products/redragon-zeusx.jpg",
             id: 10,
             comentarios: 5,
             novedad: true
         },{
-            titulo: "Mouse Logitech G903 Lightspeed Wireless Gaming 16.000dpi",
+            titulo: "Mouse Logitech G903",
             descripcion: "Logitech diseña productos y experiencias que ocupan un lugar cotidiano en la vida de las personas, poniendo foco en la innovación y la calidad. Su objetivo es crear momentos verdaderamente únicos y significativos para sus usuarios. Los mouses Logitech se adaptan a la forma de tu mano para proporcionarte horas de comodidad. Sin necesidad de mover el brazo para deslizar el cursor, tu mano se fatigará menos. Son ideales para cualquier espacio de trabajo y quienes tienen la mesa llena de diversos objetos.",
             img: "/images/products/g903.jpg",
-            id: 10,
+            id: 20,
             comentarios: 4,
             novedad: true
         },{
-            titulo: "Mouse Redragon M710 Memeanlion Chroma RGB 10,000dpi",
+            titulo: "Mouse Redragon M710",
             descripcion: "Redragon diseña productos y experiencias que ocupan un lugar cotidiano en la vida de las personas, poniendo foco en la innovación y la calidad. Su objetivo es crear momentos verdaderamente únicos y significativos para sus usuarios. Los mouses Logitech se adaptan a la forma de tu mano para proporcionarte horas de comodidad. Sin necesidad de mover el brazo para deslizar el cursor, tu mano se fatigará menos. Son ideales para cualquier espacio de trabajo y quienes tienen la mesa llena de diversos objetos.",
             img: "/images/products/m710.jpg",
-            id: 10,
+            id: 11,
             comentarios: 7,
             novedad: true
         },{
-            titulo: "Mouse HP HyperX Pulsefire FPS PRO RGB 16.000DPI",
+            titulo: "Mouse HyperX Pulsefire",
             descripcion: "HyperX diseña productos y experiencias que ocupan un lugar cotidiano en la vida de las personas, poniendo foco en la innovación y la calidad. Su objetivo es crear momentos verdaderamente únicos y significativos para sus usuarios. Los mouses Logitech se adaptan a la forma de tu mano para proporcionarte horas de comodidad. Sin necesidad de mover el brazo para deslizar el cursor, tu mano se fatigará menos. Son ideales para cualquier espacio de trabajo y quienes tienen la mesa llena de diversos objetos.",
             img: "/images/products/pulsefire.jpg",
-            id: 10,
+            id: 12,
             comentarios: 8,
             novedad: true
         },{
-            titulo: "Mouse HP HyperX Pulsefire CORE RGB",
+            titulo: "Mouse HyperX Pulsefire CORE",
             descripcion: "HyperX diseña productos y experiencias que ocupan un lugar cotidiano en la vida de las personas, poniendo foco en la innovación y la calidad. Su objetivo es crear momentos verdaderamente únicos y significativos para sus usuarios. Los mouses Logitech se adaptan a la forma de tu mano para proporcionarte horas de comodidad. Sin necesidad de mover el brazo para deslizar el cursor, tu mano se fatigará menos. Son ideales para cualquier espacio de trabajo y quienes tienen la mesa llena de diversos objetos.",
             img: "/images/products/hp.jpg",
-            id: 10,
+            id: 13,
             comentarios: 3,
             novedad: true
         },{
-            titulo: "Teclado Redragon K550 YAMA Black Mechanical Retroiluminado RGB Español",
+            titulo: "Teclado Redragon K550",
             descripcion: "Redragon diseña productos y experiencias que ocupan un lugar cotidiano en la vida de las personas, poniendo foco en la innovación y la calidad. Su objetivo es crear momentos verdaderamente únicos y significativos para sus usuarios. Los mouses Logitech se adaptan a la forma de tu mano para proporcionarte horas de comodidad. Sin necesidad de mover el brazo para deslizar el cursor, tu mano se fatigará menos. Son ideales para cualquier espacio de trabajo y quienes tienen la mesa llena de diversos objetos.",
             img: "/images/products/k550.jpg",
-            id: 10,
+            id: 14,
             comentarios: 2,
             novedad: true
         },{
-            titulo: "Teclado Logitech G213 Prodigy RGB Gaming Inglés Internacional",
+            titulo: "Teclado Logitech G213",
             descripcion: "Logitech diseña productos y experiencias que ocupan un lugar cotidiano en la vida de las personas, poniendo foco en la innovación y la calidad. Su objetivo es crear momentos verdaderamente únicos y significativos para sus usuarios. Los mouses Logitech se adaptan a la forma de tu mano para proporcionarte horas de comodidad. Sin necesidad de mover el brazo para deslizar el cursor, tu mano se fatigará menos. Son ideales para cualquier espacio de trabajo y quienes tienen la mesa llena de diversos objetos.",
             img: "/images/products/g213.jpg",
-            id: 10,
+            id: 15,
             comentarios: 1,
             novedad: true
         },{
-            titulo: "Teclado XPG Infarex K10 RGB Lighting Mem-chanical",
+            titulo: "Teclado XPG Infarex K10",
             descripcion: "XPG diseña productos y experiencias que ocupan un lugar cotidiano en la vida de las personas, poniendo foco en la innovación y la calidad. Su objetivo es crear momentos verdaderamente únicos y significativos para sus usuarios. Los mouses Logitech se adaptan a la forma de tu mano para proporcionarte horas de comodidad. Sin necesidad de mover el brazo para deslizar el cursor, tu mano se fatigará menos. Son ideales para cualquier espacio de trabajo y quienes tienen la mesa llena de diversos objetos.",
             img: "/images/products/k10.jpg",
-            id: 10,
+            id: 16,
             comentarios: 2,
             novedad: false
         },{
-            titulo: "Auriculares Logitech G433 Gaming Black 7.1 PC | PS4 | Switch | XBOX",
+            titulo: "Auriculares Logitech G433",
             descripcion: "Potentes, hermosos, totales. Con retroiluminación RGB, un micrófono excepcional, construcción cómoda y robusta, y -lo más importante- una calidad de sonido arrolladora, los aclamados Zeus vuelven con más: conoce su nueva versión X H510-RGB y experimenta la totalidad.",
             img: "/images/products/g433.jpg",
-            id: 10,
-            comentarios: 18,
+            id: 17,
+            comentarios: 8,
             novedad: false
         },{
-            titulo: "Auriculares Redragon Scylla H901 PC | PS4 | Switch | XBOX",
+            titulo: "Auriculares Redragon H901",
             descripcion: "Potentes, hermosos, totales. Con retroiluminación RGB, un micrófono excepcional, construcción cómoda y robusta, y -lo más importante- una calidad de sonido arrolladora, los aclamados Zeus vuelven con más: conoce su nueva versión X H510-RGB y experimenta la totalidad.",
             img: "/images/products/h901.jpg",
-            id: 10,
-            comentarios: 18,
+            id: 18,
+            comentarios: 7,
             novedad: false
         },{
-            titulo: "Auriculares Logitech G935 LightSync Inalambrico 7.1 RGB",
+            titulo: "Auriculares Logitech G935",
             descripcion: "Potentes, hermosos, totales. Con retroiluminación RGB, un micrófono excepcional, construcción cómoda y robusta, y -lo más importante- una calidad de sonido arrolladora, los aclamados Zeus vuelven con más: conoce su nueva versión X H510-RGB y experimenta la totalidad.",
             img: "/images/products/g935.jpg",
-            id: 10,
-            comentarios: 18,
+            id: 19,
+            comentarios: 3,
             novedad: false
         }
     ],
