0a32f587fa547d25c0fb2d90646d30c21dbd32f1
OctaSV <osonciniviola@udesa.edu.ar>
2022-04-12 15:11:36 -0300

    css y link header

--- myApp/routes/products.js
+++ myApp/routes/products.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
 
+
 router.get('/id/:id', productController.detalleProducto);
 
-router.get('/search-results', productController.create)
+router.get('/search-results', productController.create);
 
 router.get('/product-add', productController.aniadirProducto);
 
