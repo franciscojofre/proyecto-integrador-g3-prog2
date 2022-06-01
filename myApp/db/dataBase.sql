b5e067305233fec4cf0d99e031b874ca95e170a2
OctaSV <osonciniviola@udesa.edu.ar>
2022-05-31 22:35:37 -0300

    Arregle problema de la dataBase, ahora se puede crear las foreign keys

--- myApp/db/dataBase.sql
+++ myApp/db/dataBase.sql
@@ -1,8 +1,10 @@
-USE catalogo;
+CREATE SCHEMA dataBaseUdT;
+
+USE dataBaseUdT;
 
 CREATE TABLE usuario (
-/*			nombreColumna			tipoDato			restracciones							*/
-			id_usuario				INT					UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+/*			nombreColumna			tipoDato			restricciones							*/
+			id						INT					PRIMARY KEY AUTO_INCREMENT,
             nombre					VARCHAR(50)			NOT NULL,
             apellido				VARCHAR(50)			NOT NULL,
             email					VARCHAR(150)		NOT NULL,
@@ -15,37 +17,36 @@ CREATE TABLE usuario (
 );
 
 CREATE TABLE products (
-/*			nombreColumna			tipoDato			restracciones							*/
-			id_product				INT					UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+/*			nombreColumna			tipoDato			restricciones							*/
+			id						INT					PRIMARY KEY AUTO_INCREMENT,
             title					VARCHAR(50)			NOT NULL,
-            descrip					VARCHAR(50)			NOT NULL,
+            descrip					VARCHAR(1000)		NOT NULL,
             comments				INT					NOT NULL,                   
             novedad					BOOLEAN				NOT NULL,
-            image					VARCHAR(500)
-            
+            image					VARCHAR(1000),
+            user_id					INT,
+FOREIGN KEY (user_id) REFERENCES usuario(id)
 );
 
 CREATE TABLE comments (
-/*			nombreColumna			tipoDato			restracciones							*/
-			id_comment				INT					UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+/*			nombreColumna			tipoDato			restricciones							*/
+			id						INT					PRIMARY KEY AUTO_INCREMENT,
             userName				VARCHAR(50)			NOT NULL,
             user_id 				INT,
             product_id				INT,
             commentDescription		VARCHAR(1500)		NOT NULL,
             image					VARCHAR(500),
-            
-            FOREIGN KEY (user_id) REFERENCES usuario(id_usuario),
-			FOREIGN KEY (product_id) REFERENCES products(id_product)
+FOREIGN KEY (user_id) REFERENCES usuario(id),
+FOREIGN KEY (product_id) REFERENCES products(id)
 );
 
 CREATE TABLE user_follow (
-/*			nombreColumna			tipoDato			restracciones							*/
-			id_user_follow			INT					UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+/*			nombreColumna			tipoDato			restricciones							*/
+			id						INT					PRIMARY KEY AUTO_INCREMENT,
             user1_id 				INT,
             user2_id				INT,
 
             
-            FOREIGN KEY (user1_id) REFERENCES usuario(id_usuario),
-			FOREIGN KEY (user2_id) REFERENCES usuario(id_usuario)
+FOREIGN KEY (user1_id) REFERENCES usuario(id),
+FOREIGN KEY (user2_id) REFERENCES usuario(id)
 );
-
