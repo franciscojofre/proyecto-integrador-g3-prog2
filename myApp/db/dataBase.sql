e0c0028564230f70474b43f9e2504800bddefad8
OctaSV <osonciniviola@udesa.edu.ar>
2022-06-03 16:57:55 -0300

    dataBase actualizada

--- myApp/db/dataBase.sql
+++ myApp/db/dataBase.sql
@@ -1,10 +1,10 @@
-CREATE SCHEMA dataBaseUdT;
+CREATE SCHEMA dataBaseuth;
 
-USE dataBaseUdT;
+USE dataBaseuth;
 
 CREATE TABLE usuario (
 /*			nombreColumna			tipoDato			restricciones							*/
-			id						INT					PRIMARY KEY AUTO_INCREMENT,
+			id						INT					UNSIGNED PRIMARY KEY AUTO_INCREMENT,
             nombre					VARCHAR(50)			NOT NULL,
             apellido				VARCHAR(50)			NOT NULL,
             email					VARCHAR(150)		NOT NULL,
@@ -12,39 +12,39 @@ CREATE TABLE usuario (
             contrasenia				VARCHAR(50)			NOT NULL,
             fechaNacimiento			DATE 				NOT NULL,
             numeroDocumento			INT					NOT NULL,
-            fotoPerfil				VARCHAR(500)
+            fotoPerfil				VARCHAR(1000)
             
 );
 
 CREATE TABLE products (
 /*			nombreColumna			tipoDato			restricciones							*/
-			id						INT					PRIMARY KEY AUTO_INCREMENT,
+			id						INT					UNSIGNED PRIMARY KEY AUTO_INCREMENT,
             title					VARCHAR(50)			NOT NULL,
             descrip					VARCHAR(1000)		NOT NULL,
             comments				INT					NOT NULL,                   
             novedad					BOOLEAN				NOT NULL,
             image					VARCHAR(1000),
-            user_id					INT,
+            user_id					INT 				UNSIGNED,
 FOREIGN KEY (user_id) REFERENCES usuario(id)
 );
 
 CREATE TABLE comments (
 /*			nombreColumna			tipoDato			restricciones							*/
-			id						INT					PRIMARY KEY AUTO_INCREMENT,
+			id						INT					UNSIGNED PRIMARY KEY AUTO_INCREMENT,
             userName				VARCHAR(50)			NOT NULL,
-            user_id 				INT,
-            product_id				INT,
+            user_id 				INT                 UNSIGNED,
+            product_id				INT                 UNSIGNED,
             commentDescription		VARCHAR(1500)		NOT NULL,
-            image					VARCHAR(500),
+            image					VARCHAR(1000),
 FOREIGN KEY (user_id) REFERENCES usuario(id),
 FOREIGN KEY (product_id) REFERENCES products(id)
 );
 
 CREATE TABLE user_follow (
 /*			nombreColumna			tipoDato			restricciones							*/
-			id						INT					PRIMARY KEY AUTO_INCREMENT,
-            user1_id 				INT,
-            user2_id				INT,
+			id						INT					UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+            user1_id 				INT                 UNSIGNED,
+            user2_id				INT                 UNSIGNED,
 
             
 FOREIGN KEY (user1_id) REFERENCES usuario(id),
