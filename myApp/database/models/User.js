b3f38563394ffe32896062ece303a2f350612411
vdelpino <vdelpino@udesa.edu.ar>
2022-06-11 14:45:58 -0300

    Hice los models faltantes, y modifique la base de datos

---
+++ myApp/database/models/User.js
@@ -0,0 +1,48 @@
+module.exports = function (sequelize, dataTypes){
+    
+    let alias = 'User'; //Este alias se busca como nombre en de la tabla en plural dentro de la base de datos.
+
+    let cols = {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: dataTypes.INTEGER
+        },
+        nombre: {
+            type: dataTypes.STRING
+        },
+        apellido: {
+            type: dataTypes.STRING
+        },
+        email: {
+            type: dataTypes.STRING
+        },
+        username: {
+            type: dataTypes.STRING
+        },
+        contrasenia:{
+            type : dataTypes.STRING
+        },
+        fechaNacimiento : {
+            type: dataTypes.DATE,
+        },
+        numeroDocumento: {
+            type: dataTypes.INTEGER,
+        },
+        fotoPerfil: {
+            type: dataTypes.STRING
+        }
+
+    }
+
+    let config = {
+        tableName : "users",
+        timestamps:true, //Aclareción en caso de no explicitar created_at, deleted_at y updated_at
+        underscored: false, //Aclareción en caso que los timestamps usen guiones bajos en lugar de camelCase.
+    };
+
+    const User = sequelize.define(alias, cols, config);
+
+    return User;
+
+}
