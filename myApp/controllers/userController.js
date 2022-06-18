7601219e8c2d479530ad54389352ab3b0728ac04
OctaSV <osonciniviola@udesa.edu.ar>
2022-06-18 19:59:46 -0300

    Validación register final y  modificación de la database

--- myApp/controllers/userController.js
+++ myApp/controllers/userController.js
@@ -78,53 +78,73 @@ const userController = {
     },
     processRegister: (req, res) => {
         let info = req.body;
-        let fotoPerfil = req.file;  //ver esto si no se le debe agregar .filename
-        
         let errors = {}
+        let filter = {where : [
+            {email: info.email},
+            {numero_documento: info.numeroDocumento}
+        ]};
 
-        if (info.nombre == '') {
-            errors.message = 'El nombre esta vacío'
-            res.locals.errors = errors
-            return res.render('register')
-        } else if (info.apellido == ''){
-            errors.message = 'El apellido esta vacío'
-            res.locals.errors = errors
-            return res.render('register')
-        } else if (info.email == ''){
-            errors.message = 'El email esta vacío'
-            res.locals.errors = errors
-            return res.render('register')
-        } else if (info.contrasenia == ''){
-            errors.message = 'La contraseña esta vacía'
-            res.locals.errors = errors
-            return res.render('register')
-        } else if (info.fechaNacimiento === ''){
-            errors.message = 'La fecha de nacimiento esta vacía'
-            res.locals.errors = errors
-            return res.render('register')
-        } else if (info.numeroDocumento == ''){
-            errors.message = 'El numero de documento esta vacío'
-            res.locals.errors = errors
-            return res.render('register')
-        } else{
-            let dataUser = {
-                nombre: info.nombre,
-                apellido: info.apellido,
-                email: info.email,
-                contrasenia: bcrypt.hashSync(info.contrasenia, 10),
-                fecha_nacimiento: info.fechaNacimiento,
-                created_at: new Date(),
-                numero_documento: info.numeroDocumento,
-                foto_perfil: fotoPerfil
+        userModel.findOne(filter)
+        .then((result) =>{
+            if (result != null){
+                errors.message = `El email ${result.email} se encuentra en uso`
+                res.locals.errors = errors
+                return res.render('register')
+            } else {
+                if (info.nombre == '') {
+                    errors.message = 'El nombre esta vacío'
+                    res.locals.errors = errors
+                    return res.render('register')
+                } else if (info.apellido == ''){
+                    errors.message = 'El apellido esta vacío'
+                    res.locals.errors = errors
+                    return res.render('register')
+                } else if (info.email == ''){
+                    errors.message = 'El email esta vacío'
+                    res.locals.errors = errors
+                    return res.render('register')
+                } else if (info.contrasenia == ''){
+                    errors.message = 'La contraseña esta vacía'
+                    res.locals.errors = errors
+                    return res.render('register')
+                }   else if (info.contrasenia.length < 3) {
+                    errors.message = 'La contraseña no contiene más de tres dígitos'
+                    res.locals.errors = errors
+                    return res.render('register')
+                } else if (info.fechaNacimiento === ''){
+                    errors.message = 'La fecha de nacimiento esta vacía'
+                    res.locals.errors = errors
+                    return res.render('register')
+                } else if (info.numeroDocumento == ''){
+                    errors.message = 'El numero de documento esta vacío'
+                    res.locals.errors = errors
+                    return res.render('register')
+                } else{
+                    let fotoPerfil = req.file.filename;  //ver esto si no se le debe agregar .filename
+                    let dataUser = {
+                        nombre: info.nombre,
+                        apellido: info.apellido,
+                        email: info.email,
+                        contrasenia: bcrypt.hashSync(info.contrasenia, 10),
+                        fecha_nacimiento: info.fechaNacimiento,
+                        created_at: new Date(),
+                        numero_documento: info.numeroDocumento,
+                        foto_perfil: fotoPerfil
+                    }
+                    userModel.create(dataUser)
+                    .then((result) => {
+                        return res.redirect('/user/login')    
+                    })
+                    .catch((err) => {
+                        return res.send('El error es: ' + err)
+                    });
+                }
             }
-            userModel.create(dataUser)
-            .then((result) => {
-                return res.redirect('/user/login')    
-            })
-            .catch((err) => {
-                return res.send('El error es: ' + err)
-            });
-        }
+        })
+        .catch((err) => {
+            console.log('El error es: ' + err)
+        })
+
     }
 }
 
